a4e7ce0f9a4652f4d05ec03dcf05485b296afcd9
vipinkt <ktvipin27@gmail.com>
2021-12-02 04:56:06 +0530

    history screen converted to compose

--- app/src/main/java/com/whatshelp/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/whatshelp/ui/history/HistoryFragment.kt
@@ -1,63 +1,58 @@
 package com.whatshelp.ui.history
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
-import android.widget.LinearLayout
+import android.view.*
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.RecyclerView
 import com.whatshelp.R
-import com.whatshelp.databinding.FragmentHistoryBinding
+import com.whatshelp.data.db.entity.History
 import com.whatshelp.manager.analytics.AnalyticsEvent
-import com.whatshelp.ui.base.DBFragment
+import com.whatshelp.ui.base.BaseFragment
 import com.whatshelp.util.Constants.EXTRA_NUMBER
-import com.whatshelp.util.SwipeToDeleteCallback
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
-class HistoryFragment : DBFragment<FragmentHistoryBinding, HistoryViewModel>() {
+class HistoryFragment : BaseFragment<HistoryViewModel>() {
 
     override val viewModel: HistoryViewModel by viewModels()
 
-    override fun getLayoutResource(): Int = R.layout.fragment_history
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(requireContext()).apply {
+        setContent {
+            HistoryScreen { history ->
+                onClickHistory(history)
+            }
+        }
+    }
 
-    @Inject
-    lateinit var historyAdapter: HistoryAdapter
+    private fun onClickHistory(history: History) {
+        findNavController().let { navController ->
+            analyticsManager.logEvent(AnalyticsEvent.History.Click)
+            navController.previousBackStackEntry?.savedStateHandle?.set(
+                EXTRA_NUMBER,
+                history.whatsAppNumber.fullNumber
+            )
+            navController.navigateUp()
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        historyAdapter.setItemClickListener { history ->
-            findNavController().let { navController ->
-                analyticsManager.logEvent(AnalyticsEvent.History.Click)
-                navController.previousBackStackEntry?.savedStateHandle?.set(EXTRA_NUMBER,
-                    history.whatsAppNumber.fullNumber)
-                navController.navigateUp()
-            }
-        }
-
-        binding.rvHistory.apply {
-            addItemDecoration(DividerItemDecoration(requireContext(), LinearLayout.VERTICAL))
-            adapter = historyAdapter
-        }
-
         viewModel.history.observe(viewLifecycleOwner, { history ->
-            historyAdapter.submitList(history)
             setHasOptionsMenu(history.isNotEmpty())
         })
 
-        ItemTouchHelper(object : SwipeToDeleteCallback(requireContext()) {
+        /*ItemTouchHelper(object : SwipeToDeleteCallback(requireContext()) {
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 viewModel.deleteHistory(viewHolder.bindingAdapterPosition)
                 analyticsManager.logEvent(AnalyticsEvent.History.Delete)
             }
-        }).attachToRecyclerView(binding.rvHistory)
+        }).attachToRecyclerView(binding.rvHistory)*/
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
---
+++ app/src/main/java/com/whatshelp/ui/history/HistoryScreen.kt
@@ -0,0 +1,96 @@
+package com.whatshelp.ui.history
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.whatshelp.R
+import com.whatshelp.data.db.entity.History
+import com.whatshelp.ui.theme.GreyInfo
+import com.whatshelp.ui.theme.WhatsHelpTheme
+
+/**
+ * Created by Vipin KT on 02/12/21
+ */
+@Composable
+fun HistoryScreen(
+    viewModel: HistoryViewModel = viewModel(),
+    onClickHistoryItem: (History) -> Unit
+) {
+    val listState = rememberLazyListState()
+    val history by viewModel.history.observeAsState(emptyList())
+
+    WhatsHelpTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            if (history.isNotEmpty()) {
+                Text(
+                    text = stringResource(R.string.info_tap_to_select),
+                    color = MaterialTheme.colors.primary,
+                    fontSize = 14.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(GreyInfo)
+                        .padding(10.dp)
+                )
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    state = listState
+                ) {
+                    itemsIndexed(history) { index, item ->
+                        HistoryItem(item, onClickHistoryItem)
+                        if (index < history.lastIndex)
+                            Divider(color = GreyInfo, thickness = (0.5).dp)
+                    }
+                }
+            } else {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = stringResource(R.string.message_no_data),
+                        fontSize = 14.sp
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun HistoryItem(history: History, onClickItem: (History) -> Unit) {
+    Surface(
+        onClick = { onClickItem(history) }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(70.dp)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = history.formattedNumber
+            )
+        }
+    }
+}
